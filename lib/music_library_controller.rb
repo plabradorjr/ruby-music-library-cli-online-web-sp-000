04ad49f0ad9f620ec2f78a05dc7eee0a9c3f9a7f
Pancho Labrador <plabradorjr@gmail.com>
2019-11-12 14:39:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,6 +6,10 @@ class MusicLibraryController
 
   def call
 
+    input = ""
+
+    while input != "exit"
+      
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -15,6 +19,11 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+
+    input = gets.strip
+
+    case input
+      
 
   end
 
