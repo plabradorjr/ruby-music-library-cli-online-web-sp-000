e07ef4a2ea2f554c466d766fd1db36f17a376493
Pancho Labrador <plabradorjr@gmail.com>
2019-11-12 13:03:15 +0000

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,21 @@
+class MusicImporter
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
+
+  def import
+    files.each{ |f| Song.create_from_filename(f) }
+  end
+
+end
